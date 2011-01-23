2f9b7f3de7e9bcbbb5d1cc740f9c4373a8014ab0
Andrew Cox <drew.cox@gmail.com>
2011-01-23 12:59:47 -0500

    Added uniqueness validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   validates_confirmation_of :password
   validates_length_of :password, :minimum => 4, :allow_blank => true
+  validates_uniqueness_of :username
 
   def self.authenticate(login, pass)
     user = find_by_username(login)
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,8 +7,7 @@ describe User do
         :username => 'cox',
         :password => 'password',
         :password_confirmation => 'password')
-      cox.save
-      cox.errors.size.should == 0
+      cox.save.should be_true
     end
 
     it "fails with a password less than 4 characters" do
@@ -20,15 +19,40 @@ describe User do
       cox.errors.size.should == 1
       cox.errors[:password].first.should match /minimum is 4 characters/
     end
+
+    it "fails if username is already taken" do
+      andrew_cox = User.create(
+        :username => 'cox',
+        :password => 'password',
+        :password_confirmation => 'password')
+      anne_cox = User.create(
+        :username => 'cox',
+        :password => 'awesome',
+        :password_confirmation => 'awesome')
+      anne_cox.errors[:username].first.should match /has already been taken/
+    end
   end
 
   context "authentication" do
+    before(:each) do
+      @username = 'cox'
+      @password = 'sausage'
+      @cox = User.create(
+        :username => @username,
+        :password => @password,
+        :password_confirmation => @password)
+    end
+
     it "authenticates with a valid username and password" do
-      User.create(
-        :username => 'cox',
-        :password => 'sausage',
-        :password_confirmation => 'sausage')
-      User.authenticate('cox', 'sausage').should be_a_kind_of User
+      User.authenticate(@username, @password).should be_a_kind_of User
+    end
+
+    it "can determine a matching passsword" do
+      @cox.matching_password?(@password).should be_true
+    end
+
+    it "can determine an unmatching passsword" do
+      @cox.matching_password?("#{@password}_bogus").should be_false
     end
   end
 end
