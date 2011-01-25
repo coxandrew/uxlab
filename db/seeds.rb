7415897f07a14aea4b441bbf835594bc2b93baf7
Andrew Cox <drew.cox@gmail.com>
2011-01-25 07:16:19 -0500

    Adding more default data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,10 @@ end
 uni_web = Project.create(:name => "University Web",
   :description => "School administration app for Ruby Mendicant University")
 admin_users = Feature.create(:name => "User Administration",
+  :description => "Add, edit and delete users. Assign permissions. Track students' progress throughout a course.",
   :project_id => uni_web.id)
 admin_courses = Feature.create(:name => "Course Administration",
+  :description => "Create and manage courses. Add students and instructors to a course.",
   :project_id => uni_web.id)
 
 Flow.create(:name => "Add user to RMU",
@@ -26,3 +28,12 @@ Flow.create(:name => "Add users to course",
 
 ux_lab = Project.create(:name => "UX Lab",
   :description => "A resource manager for storing and sharing design images and documents for a project.")
+Feature.create(:name => "Projects",
+  :description => "A project is the main organizational element that contains all of your features.",
+  :project_id => ux_lab.id)
+Feature.create(:name => "Features",
+  :description => "Group your project into meaningful, MMFs (minimally marketable features).",
+  :project_id => ux_lab.id)
+Feature.create(:name => "User Flows",
+  :description => "Add flows from the user's perspective for all important UI interactions. Flows will include a set of re-orderable screenshots.",
+  :project_id => ux_lab.id)
