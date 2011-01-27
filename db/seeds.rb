7b79fd0a31272d2ee9d2495e8b498385d93d3747
Andrew Cox <drew.cox@gmail.com>
2011-01-26 22:13:26 -0500

    Tweaked CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-
 user_defaults = { :password => 'tronguy', :password_confirmation => 'tronguy' }
-users = ['cox', 'jordanbyron', 'seacreature']
+users = ['cox', 'jordanbyron', 'seacreature', 'tronguy']
 users.each do |user|
   User.create(user_defaults.merge(:username => user))
 end
 
+# University Web
 uni_web = Project.create(:name => "University Web",
   :description => "School administration app for Ruby Mendicant University")
 admin_users = Feature.create(:name => "User Administration",
@@ -26,6 +19,7 @@ Flow.create(:name => "Add user to RMU",
 Flow.create(:name => "Add users to course",
   :feature_id => admin_courses.id)
 
+# UX Lab
 ux_lab = Project.create(:name => "UX Lab",
   :description => "A resource manager for storing and sharing design images and documents for a project.")
 Feature.create(:name => "Projects",
